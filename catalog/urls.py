3d0e237d38b381433b21914c3ba5fa7c4f3dd5bc
Anton Parshikov <tosha1703@gmail.com>
2023-07-11 13:59:22 +0300

    add urls and views

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from catalog.views import home, contacts
+from . import views
+
+urlpatterns = [
+    path('', home),
+    path('contacts/', views.contacts, name='contacts')
+
+]
